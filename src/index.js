46f03072dfdab67f9eb4ac571bf3a8e0be9f58c5
Muriithi Ian <imuriithiian@gmail.com>
2022-08-30 15:29:47 +0300

    Show first character on load

--- src/index.js
+++ src/index.js
@@ -1,1 +1,37 @@
 // Your code here
+/* targets */
+const charactersBar = document.querySelector("#character-bar");
+const characterName = document.querySelector("#name")
+const characterImage = document.querySelector("#image");
+const characterVotes = document.querySelector("#vote-count");
+const characterVotesForm = document.querySelector("#votes-form");
+const characterVotesFormInput = document.querySelector("#votes");
+const characterVotesReset = document.querySelector("#reset-btn");
+
+function fetchData(id = null) {
+  const url = id
+    ? ` http://localhost:3000/characters/${id}`
+    : "http://localhost:3000/characters";
+  return fetch(url)
+    .then((res) => res.json())
+    .catch((err) => console.log(err));
+}
+
+function renderCharacterBar(characters) {
+    
+
+}
+
+function renderCharacter(character) {
+  //data render
+  characterName.textContent = character.name;
+    characterImage.src = character.image;
+    characterImage.alt = character.name;
+    characterVotes.textContent = character.votes;
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+    fetchData(1).then(character=>{
+        renderCharacter(character)
+    })
+})
